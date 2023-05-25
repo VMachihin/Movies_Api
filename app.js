44fe734a9e7c1681edce9995d88a7582b3aa0203
VitMach <machihin_vitalii@mail.ru>
2023-05-25 23:06:29 +0300

    Работа с api диплома 1 level\

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const centralizedErrorHandler = require('./errors/centralized-error-handler');
 const { NotFoundErr } = require('./errors');
 const { errors } = require('celebrate');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
+const cors = require('./middlewares/cors');
 
 const app = express();
 const { PORT = 3000 } = process.env;
@@ -17,6 +18,7 @@ app.use(express.json()); // для собирания JSON-формата
 app.use(express.urlencoded({ extended: true })); // для приёма веб-страниц внутри POST-запроса
 
 app.use(requestLogger);
+app.use(cors);
 
 app.use(router);
 
