a40c21e9dfe18acdb8ae510058952d84b7b22a23
VitMach <machihin_vitalii@mail.ru>
2023-05-26 10:06:46 +0300

    Работа с api диплома 1 level\

--- validation/movie.js
+++ validation/movie.js
@@ -3,17 +3,17 @@ const urlValidate = /^(http|https):\/\/[^ "]+$/;
 
 const createMovieValidation = {
   body: Joi.object({
-    country: Joi.string().min(2).max(30).required(),
-    director: Joi.string().min(2).max(30).required(),
-    duration: Joi.number().min(2).max(30).required(),
-    year: Joi.string().min(2).max(30).required(),
-    description: Joi.string().min(2).max(30).required(),
-    image: Joi.string().regex(urlValidate),
-    trailerLink: Joi.string().regex(urlValidate),
-    thumbnail: Joi.string().regex(urlValidate),
+    country: Joi.string().required(),
+    director: Joi.string().required(),
+    duration: Joi.number().required(),
+    year: Joi.string().required(),
+    description: Joi.string().required(),
+    image: Joi.string().regex(urlValidate).required(),
+    trailerLink: Joi.string().regex(urlValidate).required(),
+    thumbnail: Joi.string().regex(urlValidate).required(),
     movieId: Joi.number().required(),
-    nameRU: Joi.string().min(2).max(30).required(),
-    nameEN: Joi.string().min(2).max(50).required(),
+    nameRU: Joi.string().required(),
+    nameEN: Joi.string().required(),
   }),
 };
 
