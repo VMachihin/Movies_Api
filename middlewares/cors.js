9941e805a588b895ab591786a718c22e9d9d79f5
VitMach <machihin_vitalii@mail.ru>
2023-07-15 00:00:15 +0300

    Работа с функциональностью диплома level-3 \

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -3,35 +3,47 @@
 //   'http://api.movies.lib.nomoredomains.rocks',
 //   'https://movies.lib.nomoredomains.rocks',
 //   'http://movies.lib.nomoredomains.rocks',
-//   'https://movies.lib.nomoredomains.rocks.vitmach.ru',
-//   'http://movies.lib.nomoredomains.rocks.vitmach.ru',
 //   'localhost:3000',
 //   'http://localhost:3000',
 //   'localhost:8000',
 //   'http://localhost:8000',
 // ];
 
+// module.exports = (req, res, next) => {
+//   const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
+//   const { method } = req; // Сохраняем тип запроса (HTTP-метод) в соответствующую переменную
+//   const requestHeaders = req.headers['access-control-request-headers'];
+//   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+//   // проверяем, что источник запроса есть среди разрешённых
+//   if (allowedCors.includes(origin)) {
+//     // устанавливаем заголовок, который разрешает браузеру запросы с этого источника
+//     // res.header('Access-Control-Allow-Origin', origin);
+//     // устанавливаем заголовок, который разрешает браузеру запросы из любого источника
+//     res.header('Access-Control-Allow-Origin', '*');
+//   }
+
+//   if (method === 'OPTIONS') {
+//     // разрешаем кросс-доменные запросы любых типов (по умолчанию)
+//     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+//     // разрешаем кросс-доменные запросы с этими заголовками
+//     res.header('Access-Control-Allow-Headers', requestHeaders);
+//     // завершаем обработку запроса и возвращаем результат клиенту
+//     return res.end();
+//   }
+
+//   return next();
+// };
+
 module.exports = (req, res, next) => {
-  // const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
-  const { method } = req; // Сохраняем тип запроса (HTTP-метод) в соответствующую переменную
+  const { method } = req;
   const requestHeaders = req.headers['access-control-request-headers'];
   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
-  // проверяем, что источник запроса есть среди разрешённых
-  // if (allowedCors.includes(origin)) {
-  //   // устанавливаем заголовок, который разрешает браузеру запросы с этого источника
-  //   // res.header('Access-Control-Allow-Origin', origin);
-  //   // устанавливаем заголовок, который разрешает браузеру запросы из любого источника
-  //   res.header('Access-Control-Allow-Origin', '*');
-  // }
 
   res.header('Access-Control-Allow-Origin', '*');
 
   if (method === 'OPTIONS') {
-    // разрешаем кросс-доменные запросы любых типов (по умолчанию)
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-    // разрешаем кросс-доменные запросы с этими заголовками
     res.header('Access-Control-Allow-Headers', requestHeaders);
-    // завершаем обработку запроса и возвращаем результат клиенту
     return res.end();
   }
 
